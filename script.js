0ce56ce6bd2d3249b16a9b738f0b91b0a74d0a25
Anuj Sharma <anujsharmasppu@gmail.com>
2022-05-28 11:53:02 +0530

    Add the project

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-		
+function onDownload() {
+  const fe = document.querySelector('input[name="selector-fe"]:checked').value;
+  const be = document.querySelector('input[name="selector-be"]:checked').value;
+  window.open("https://frozen-cabbage-backend.herokuapp.com/templates/download?backend="+be, "_blank");
+  window.open("https://frozen-cabbage-backend.herokuapp.com/templates/download?frontend="+fe, "_blank");
+}
